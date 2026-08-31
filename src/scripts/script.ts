17548df7119288bf41ee4ca3fd19df1c9b55fd33
cr1ttr <isabelle.hall04@gmail.com>
2026-08-31 15:49:11 -0500

    feat/board-setup: modified how the DOM is built to match the css rules

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -2,19 +2,26 @@ function createBoard(): HTMLElement {
     const board: HTMLElement = document.createElement("div");
     board.classList.add("board");
     document.body.appendChild(board);
+    const pieceContainer: HTMLElement = document.createElement("div");
+    pieceContainer.classList.add("piece-container");
+    board.appendChild(pieceContainer);
     return board;
 }
 
 function addSquares(board: HTMLElement) {
+
+    const cellContainer: HTMLElement = document.createElement("div");
+    cellContainer.classList.add("cell-container"); 
+    board.appendChild(cellContainer);
+
     for (let y: number = 0; y < 8; y++) {
         for (let x: number = 0; x < 8; x++) {
             const sq: HTMLElement = document.createElement("div");
-            sq.classList.add((x + y) % 2 ? "sq-dark" : "sq-light");
-            board.appendChild(sq)
+            sq.classList.add(...["sq", (x + y) % 2 ? "dark" : "light"]);
+            cellContainer.appendChild(sq)
         }
     }
 }
-
 
 document.addEventListener("DOMContentLoaded", () => {
     const board: HTMLElement = createBoard();
