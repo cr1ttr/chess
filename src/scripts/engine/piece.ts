6f1ef87626f32f03458601501013fa97af121a34
Isabelle <121070808+cr1ttr@users.noreply.github.com>
2026-09-08 18:17:48 -0500

    refactor/basic-data-structures: propagated the piece file

--- src/scripts/engine/piece.ts
+++ src/scripts/engine/piece.ts
@@ -1,3 +1,12 @@
+export type PieceKind = 'pawn' | 'knight' | 'bishop' | 'rook' | 'queen' | 'king';
+export type PieceTeam = 'black' | 'white';
+
 export default class Piece {
-    
+    kind: PieceKind;
+    team: PieceTeam;
+
+    constructor(kind: PieceKind, team: PieceTeam) {
+        this.kind = kind;
+        this.team = team;
+    }
 }
