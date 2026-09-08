05670004644da158d6e69b18ecafc6e20c9fbd8b
Isabelle <121070808+cr1ttr@users.noreply.github.com>
2026-09-08 18:19:51 -0500

    refactor/basic-data-structures: propagated the game file

--- src/scripts/engine/game.ts
+++ src/scripts/engine/game.ts
@@ -1,21 +1,30 @@
-import Board from "./board"
-import CastlingRights from "./castling_rights";
-import Timer from "./timer";
-// import EventEmitter from "./event_emitter";
+import Board from "./board.js"
+import CastlingRights from "./castling_rights.js";
+import FEN from "./fen.js";
+import Timer from "./timer.js";
+import Vector2 from "./vector2.js";
 
 export default class Game {
-    state: Board;
+    state: Board = new Board();
     blackCastlingRights: CastlingRights;
     whiteCastlingRights: CastlingRights;
-    whiteTimer: Timer;
-    blackTimer: Timer;
+    whiteTimer: Timer | undefined = undefined;
+    blackTimer: Timer | undefined = undefined;
+    turnToMove: 'black' | 'white';
+    halfMoves: number;
+    fullMoves: number;
+    enPassantSquare: Vector2 | undefined;
 
 
     constructor() {
-        this.state = new Board();
+        FEN.loadPosition("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", this);
         this.blackCastlingRights = new CastlingRights({kingSide: true, queenSide: true});
         this.whiteCastlingRights = new CastlingRights({kingSide: true, queenSide: true});
-        this.whiteTimer = new Timer();
-        this.blackTimer = new Timer();
+        this.whiteTimer = undefined;
+        this.blackTimer = undefined;
+        this.turnToMove = 'white';
+        this.halfMoves = 0;
+        this.fullMoves = 1;
+        this.enPassantSquare = undefined;
     }
 }
