8fc7d5be4c5d2c40feb876101f7246bfa01081b2
Isabelle <121070808+cr1ttr@users.noreply.github.com>
2026-09-08 18:17:33 -0500

    refactor/basic-data-structures: propagated the board file

--- src/scripts/engine/board.ts
+++ src/scripts/engine/board.ts
@@ -1,3 +1,17 @@
-export default class Board {
+import type Piece from "./piece.js";
 
+export default class Board {
+    public static readonly WIDTH: number = 8;
+    public static readonly HEIGHT: number = 8;
+
+    squares: Array<Array<Piece | null>> = [];
+
+
+    constructor() {
+        for (let y = 0; y < Board.HEIGHT; y++) {
+            let tmp: Array<Piece | null> = [];
+            for (let x = 0; x < Board.WIDTH; x++) tmp.push(null);
+            this.squares.push(tmp);
+        }
+    }
 }
